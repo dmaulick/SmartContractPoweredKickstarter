c10f96b95b0b3a7fb413cc283eddecf9610cf44d
david.maulick <david.maulick@coinbase.com>
2020-10-07 19:05:12 -0700

    add compile file

---
+++ ethereum/compile.js
@@ -0,0 +1,88 @@
+const path = require('path');
+const solc = require('solc');
+const fs = require('fs-extra');
+
+// __dirname == is the current working directory
+const buildPath = path.resolve(__dirname, 'build');
+fs.removeSync(buildPath);
+
+const campaignPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
+const source = fs.readFileSync(campaignPath, 'utf8');
+
+// Docs on solc
+// https://solidity.readthedocs.io/en/v0.5.0/using-the-compiler.html#compiler-input-and-output-json-description
+var input = {
+  language: 'Solidity',
+  sources: {
+    'Campaign.sol': {
+      content: source
+    }
+  },
+  settings: {
+    outputSelection: {
+      '*': {
+        '*': ['*']
+      }
+    }
+  }
+};
+ 
+const output = JSON.parse(solc.compile(JSON.stringify(input)));
+const contracts = output.contracts['Campaign.sol'];
+
+fs.ensureDirSync(buildPath);
+
+
+for (let key in contracts) {
+	fs.outputJsonSync(
+		path.resolve(buildPath, key +'.json'),
+		contracts[key],
+		{
+			spaces: 3
+		}
+	);
+}
+
+
+// For testing the attributes of the contracts
+// const contractValues = Object.values(contracts);
+// const contractEntries = Object.entries(contracts);
+// const contractKeys = Object.keys(contracts);
+// fs.outputJsonSync(
+// 		path.resolve(buildPath, 'contracts' +'.json'),
+// 		contracts,
+// 		{
+// 			spaces: 3
+// 		}
+// 	);
+
+// fs.outputJsonSync(
+// 		path.resolve(buildPath, 'entries' +'.json'),
+// 		contractEntries,
+// 		{
+// 			spaces: 3
+// 		}
+// 	);
+
+// fs.outputJsonSync(
+// 		path.resolve(buildPath, 'values' +'.json'),
+// 		contractValues,
+// 		{
+// 			spaces: 3
+// 		}
+// 	);
+
+// fs.outputJsonSync(
+// 		path.resolve(buildPath, 'keys' +'.json'),
+// 		contractKeys,
+// 		{
+// 			spaces: 3
+// 		}
+// 	);
+// 
+// contractEntries.forEach((element) => {
+// 	fs.outputJsonSync(
+// 		path.resolve(buildPath, element.key +'.json'),
+// 		element.value
+// 	);
+// })
--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -1,4 +1,5 @@
-pragma solidity >=0.4.22 <0.7.0;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.12 <=0.6.12;
 
 contract CampaignFactory {
     address[] public deployedCampaigns;
